8463aba3008abb450182ae8ed8536fe8ec26166e
Nihat Çetin <96945187+nihatctnn@users.noreply.github.com>
2024-02-17 21:25:45 +0300

    Add files via upload

---
+++ big-integers/src/Main.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = scan.nextBigInteger();
+        BigInteger b = scan.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+
+    }
+}
